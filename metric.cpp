5edaa08962b358cc1587fe988656f5bb998ff045
glashunti <gleibsonglas2@yahoo.com.br>
2017-04-20 23:39:42 -0300

    Firs
    commit

---
+++ metric.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+	long double weight;
+	long double ton = 35273.92;
+
+	std::cout << "Insert the weight of the breakfast package in ounces: ";
+	std::cin >> weight;
+	std::cout << "The weight of your package in ton is: " << weight / ton << std::endl;
+
+
+	return 0;
+
+
+
+
+
+
+
+}
